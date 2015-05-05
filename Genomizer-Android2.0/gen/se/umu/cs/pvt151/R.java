01cea39342aa77a38d647023f3ab7fd3d117693a
ens11pnn <ens11pnn@sirius.cs.umu.se>
2015-05-05 10:21:10 +0200

    rm bin

--- Genomizer-Android2.0/gen/se/umu/cs/pvt151/R.java
+++ Genomizer-Android2.0/gen/se/umu/cs/pvt151/R.java
@@ -14,7 +14,14 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int black=0x7f040003;
+        public static final int black_login=0x7f040005;
         public static final int blue=0x7f040000;
+        public static final int gray_login=0x7f040006;
+        public static final int light_gray=0x7f040002;
+        public static final int light_gray_experimentFiles=0x7f040007;
+        public static final int smoke_white=0x7f040004;
+        public static final int white=0x7f040001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -35,37 +42,51 @@ public final class R {
         public static final int tools=0x7f020004;
     }
     public static final class id {
+        public static final int RelativeLayout1=0x7f0a000d;
         public static final int item_detail_container=0x7f0a000c;
-        public static final int login_action_settings=0x7f0a0014;
+        public static final int listView1=0x7f0a0010;
+        public static final int listView2=0x7f0a0015;
+        public static final int listView3=0x7f0a0013;
+        public static final int login_action_settings=0x7f0a0020;
         public static final int login_btn_signIn=0x7f0a0001;
         public static final int login_et_enterPassword=0x7f0a0004;
         public static final int login_et_enterUsername=0x7f0a0003;
         public static final int login_iv_genomizer=0x7f0a0006;
         public static final int login_layout_inputField=0x7f0a0002;
         public static final int login_layout_main=0x7f0a0000;
-        public static final int login_settings_addURL=0x7f0a0016;
+        public static final int login_settings_addURL=0x7f0a0022;
         public static final int login_settings_btn_saveURL=0x7f0a000a;
         public static final int login_settings_et_inputField=0x7f0a0009;
-        public static final int login_settings_removeURL=0x7f0a0015;
+        public static final int login_settings_removeURL=0x7f0a0021;
         public static final int login_settings_spinner_servers=0x7f0a0008;
         public static final int login_settings_tv_selectAServerURL=0x7f0a0007;
         public static final int login_sv=0x7f0a0005;
-        public static final int realtabcontent=0x7f0a000d;
-        public static final int searchListView=0x7f0a0013;
-        public static final int search_btn_search=0x7f0a0012;
-        public static final int search_layout=0x7f0a0011;
-        public static final int search_pubmed_button_search=0x7f0a0010;
-        public static final int search_pubmed_input_field=0x7f0a000f;
-        public static final int search_tv_pubmedStyle=0x7f0a000e;
+        public static final int realtabcontent=0x7f0a0019;
+        public static final int searchListView=0x7f0a001f;
+        public static final int search_btn_search=0x7f0a001e;
+        public static final int search_layout=0x7f0a001d;
+        public static final int search_lv_searchResultList=0x7f0a0018;
+        public static final int search_pubmed_button_search=0x7f0a001c;
+        public static final int search_pubmed_input_field=0x7f0a001b;
+        public static final int search_tv_divider=0x7f0a0017;
+        public static final int search_tv_pubmedStyle=0x7f0a001a;
+        public static final int search_tv_searchResult=0x7f0a0016;
+        public static final int sendBtn=0x7f0a0014;
         public static final int settings_btn_editURL=0x7f0a000b;
+        public static final int textFiles=0x7f0a000e;
+        public static final int textProfile=0x7f0a0011;
+        public static final int textRaw=0x7f0a000f;
+        public static final int textRegion=0x7f0a0012;
     }
     public static final class layout {
         public static final int login_layout=0x7f030000;
         public static final int login_layout_settings=0x7f030001;
         public static final int main_layout=0x7f030002;
-        public static final int search_layout_mother=0x7f030003;
-        public static final int search_layout_pubmed=0x7f030004;
-        public static final int search_layout_regular=0x7f030005;
+        public static final int search_layout_experimentfiles=0x7f030003;
+        public static final int search_layout_experimentlist=0x7f030004;
+        public static final int search_layout_mother=0x7f030005;
+        public static final int search_layout_pubmed=0x7f030006;
+        public static final int search_layout_regular=0x7f030007;
     }
     public static final class menu {
         public static final int login_menu=0x7f090000;
@@ -100,6 +121,16 @@ public final class R {
         /**  Used in search_layout_regular 
          */
         public static final int searchRegular_btn_search=0x7f06000d;
+        public static final int search_btn_addToSelection=0x7f060019;
+        /**  Used in search_layout_experimentfiles 
+         */
+        public static final int search_tv_experimentFiles=0x7f060015;
+        public static final int search_tv_profileData=0x7f060017;
+        public static final int search_tv_rawData=0x7f060016;
+        public static final int search_tv_regionData=0x7f060018;
+        /**  Used in search_layout_experimentList 
+         */
+        public static final int search_tv_searchResult=0x7f060014;
         public static final int title_activity_main=0x7f060011;
     }
     public static final class style {
